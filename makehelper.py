ee9f652c05cc12d6f51dfe5e1f9f553bb8a9f1d2
Peter Heesterman <Peter.Heesterman@Capgemini.com>
2024-08-12 11:34:54 +0100

    Added Python 3.12 as a build candidate. Changed to eddbyl's version of makehelper.py, which is likely to work for Python3.12 but maybe not for other versions. (I know this doesn't work for Windows.)

--- makehelper.py
+++ makehelper.py
@@ -26,45 +26,32 @@ else:
         pass
     out = open(sys.argv[1], 'w')
 
-from sysconfig import get_config_var
-try:
-    # NB, sysconfig.get_python_inc doesn't exist, but distutils.sysconfig.get_python_inc does for Python <= 3.11
-    from distutils.sysconfig import get_python_inc
-except ImportError:
-    def get_python_inc():
-        return get_config_var('INCLUDEPY') or ''
+from sysconfig import get_config_var, get_path, get_python_version
 
-incdirs = [get_python_inc()]
-libdirs = [
-    # distutils.sysconfig.get_python_lib does not appear to work correctly on Ubuntu with Python versions < 3.10
-    get_config_var('LIBDIR') or get_config_var('LIBDEST') or '',
-    get_config_var('BINDIR') or '',
-]
+incdirs = [get_path("include")]
+libdir = get_config_var('LIBDIR') or ''
 
 have_np='NO'
 try:
-    from numpy.distutils.misc_util import get_numpy_include_dirs
-    incdirs = get_numpy_include_dirs()+incdirs
+    from numpy import get_include
+    numpy_dir = [get_include()]
+    incdirs = numpy_dir+incdirs
     have_np='YES'
 except ImportError:
     pass
 
-print('TARGET_CFLAGS +=',get_config_var('BASECFLAGS') or '', file=out)
-print('TARGET_CXXFLAGS +=',get_config_var('BASECFLAGS') or '', file=out)
+print('TARGET_CFLAGS +=',get_config_var('BASECFLAGS'), file=out)
+print('TARGET_CXXFLAGS +=',get_config_var('BASECFLAGS'), file=out)
 
-print('PY_VER :=',get_config_var('VERSION'), file=out)
+print('PY_VER :=',get_python_version(), file=out)
 ldver = get_config_var('LDVERSION')
 if ldver is None:
-    ldver = get_config_var('VERSION')
+    ldver = get_python_version()
     if get_config_var('Py_DEBUG'):
         ldver = ldver+'_d'
 print('PY_LD_VER :=',ldver, file=out)
 print('PY_INCDIRS :=',' '.join(incdirs), file=out)
-print('PY_LIBDIRS :=',' '.join(libdirs), file=out)
-if sys.platform == 'win32':
-    print('PY_LDLIBS :=', '/LIBPATH:' + os.path.join(sys.prefix, 'libs'), file=out)
-else:
-    print('PY_LDLIBS :=', get_config_var('BLDLIBRARY') or '', file=out)
+print('PY_LIBDIRS :=',libdir, file=out)
 print('HAVE_NUMPY :=',have_np, file=out)
 
 try:
